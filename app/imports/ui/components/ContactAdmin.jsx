606ea83834d355c3bbfc1f1d5cb7608f7b2de9f2
Brandon Underwood <103099466+branndonU@users.noreply.github.com>
2023-10-31 20:34:09 -1000

    contact collections done @ 35min

--- app/imports/ui/components/ContactAdmin.jsx
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -4,34 +4,31 @@ import { Card, Image } from 'react-bootstrap';
 
 /* Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 // eslint-disable-next-line react/prop-types
-const Contact = ({ contact }) => (
+const ContactAdmin = ({ contact }) => (
   <Card className="h-100">
     <Card.Header>
-      {/* eslint-disable-next-line react/prop-types */}
       <Image src={contact.image} width={75} />
-      {/* eslint-disable-next-line react/prop-types */}
       <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
-      {/* eslint-disable-next-line react/prop-types */}
       <Card.Subtitle>{contact.address}</Card.Subtitle>
     </Card.Header>
     <Card.Body>
-      {/* eslint-disable-next-line react/prop-types */}
       <Card.Text>{contact.description}</Card.Text>
+      <footer className="blockquote-footer">{contact.owner}</footer>
     </Card.Body>
-
   </Card>
 );
 
 // Require a document to be passed to this component.
-Contact.propTypes = {
-  Contact: PropTypes.shape({
+ContactAdmin.propTypes = {
+  contact: PropTypes.shape({
     firstName: PropTypes.string,
     lastName: PropTypes.string,
     address: PropTypes.string,
     image: PropTypes.string,
     description: PropTypes.string,
+    owner: PropTypes.string,
     // _id: PropTypes.string,
   }).isRequired,
 };
 
-export default Contact;
+export default ContactAdmin;
--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -6,7 +6,6 @@ import { Roles } from 'meteor/alanning:roles';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
 import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
@@ -17,7 +16,9 @@ import SignIn from '../pages/SignIn';
 import NotAuthorized from '../pages/NotAuthorized';
 import LoadingSpinner from '../components/LoadingSpinner';
 import ListContacts from '../pages/ListContacts';
+import ListContactsAdmin from '../pages/ListContactsAdmin';
 
+/** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 const App = () => {
   const { ready } = useTracker(() => {
     const rdy = Roles.subscription.ready();
@@ -25,13 +26,12 @@ const App = () => {
       ready: rdy,
     };
   });
-
   return (
     <Router>
       <div className="d-flex flex-column min-vh-100">
         <NavBar />
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route exact path="/" element={<Landing />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signout" element={<SignOut />} />
@@ -39,7 +39,7 @@ const App = () => {
           <Route path="/list" element={<ProtectedRoute><ListContacts /></ProtectedRoute>} />
           <Route path="/add" element={<ProtectedRoute><AddStuff /></ProtectedRoute>} />
           <Route path="/edit/:_id" element={<ProtectedRoute><EditStuff /></ProtectedRoute>} />
-          <Route path="/admin" element={<AdminProtectedRoute ready={ready}><ListStuffAdmin /></AdminProtectedRoute>} />
+          <Route path="/admin" element={<AdminProtectedRoute ready={ready}><ListContactsAdmin /></AdminProtectedRoute>} />
           <Route path="/notauthorized" element={<NotAuthorized />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
@@ -49,15 +49,21 @@ const App = () => {
   );
 };
 
+/*
+ * ProtectedRoute (see React Router v6 sample)
+ * Checks for Meteor login before routing to the requested page, otherwise goes to signin page.
+ * @param {any} { component: Component, ...rest }
+ */
 const ProtectedRoute = ({ children }) => {
   const isLogged = Meteor.userId() !== null;
   return isLogged ? children : <Navigate to="/signin" />;
 };
 
-ProtectedRoute.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-};
-
+/**
+ * AdminProtectedRoute (see React Router v6 sample)
+ * Checks for Meteor login and admin role before routing to the requested page, otherwise goes to signin page.
+ * @param {any} { component: Component, ...rest }
+ */
 const AdminProtectedRoute = ({ ready, children }) => {
   const isLogged = Meteor.userId() !== null;
   if (!isLogged) {
@@ -70,9 +76,24 @@ const AdminProtectedRoute = ({ ready, children }) => {
   return (isLogged && isAdmin) ? children : <Navigate to="/notauthorized" />;
 };
 
+// Require a component and location to be passed to each ProtectedRoute.
+ProtectedRoute.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+};
+
+ProtectedRoute.defaultProps = {
+  children: <Landing />,
+};
+
+// Require a component and location to be passed to each AdminProtectedRoute.
 AdminProtectedRoute.propTypes = {
   ready: PropTypes.bool,
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+};
+
+AdminProtectedRoute.defaultProps = {
+  ready: false,
+  children: <Landing />,
 };
 
 export default App;
